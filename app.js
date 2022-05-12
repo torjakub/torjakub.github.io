8d22032790dbc4b237a2a90e8ae63bdf27ff9857
torjakub <torjakub@gmail.com>
2022-05-13 01:16:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ function setPos(position){
 }
 
 function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     
     fetch(api)
         .then(function(response){
